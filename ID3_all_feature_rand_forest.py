11bc68aa2540b207fc32b7b78edbf61cfea76549
yoyololicon <ya70201@gmail.com>
2017-10-31 22:02:04 +0800

    change readme to report.

--- ID3_all_feature_rand_forest.py
+++ ID3_all_feature_rand_forest.py
@@ -56,7 +56,7 @@ if __name__ == '__main__':
         for j in range(K):
             if j != i:
                 train+=kfold_data[j]
-        for j in range(5):
+        for j in range(10):
             forest.append(ID3_all_feature.ID3(random_forest.rand_feature(ID3_baseline.features, copy.deepcopy(feature_div), 4), random_forest.rand_data(train, int(len(train) * 0.75))))
         tp = 0
         for j in test:
